24c145b4a00711609207da9d1be78944e1a861dd
dpylak <203235923+dawpyl@users.noreply.github.com>
2026-03-31 13:28:29 +0200

    feat: enhance document characteristics component with dynamic form handling

--- src/app/document/pages/document-create/components/document-create-characteristics/document-create-characteristics.component.ts
+++ src/app/document/pages/document-create/components/document-create-characteristics/document-create-characteristics.component.ts
@@ -1,22 +1,70 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { DocumentCharacteristicFormValue } from '../../../../types/document-create.types';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { FormArray } from '@angular/forms';
+import {
+  DocumentCharacteristicFormValue,
+  DocumentCharacteristicsFormGroup,
+} from '../../../../types/document-create.types';
+import { createCharacteristicFormGroup } from '../../../../utils/document-details-form.factory';
 
 @Component({
   selector: 'app-document-create-characteristics',
   templateUrl: './document-create-characteristics.component.html',
   styleUrls: ['./document-create-characteristics.component.scss'],
 })
-export class DocumentCreateCharacteristicsComponent {
+export class DocumentCreateCharacteristicsComponent implements OnInit {
   @Input() characteristics: DocumentCharacteristicFormValue[] = [];
 
   @Output() back = new EventEmitter<void>();
   @Output() save = new EventEmitter<DocumentCharacteristicFormValue[]>();
 
+  readonly characteristicsForm =
+    new FormArray<DocumentCharacteristicsFormGroup>([]);
+
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
   onBackClick(): void {
     this.back.emit();
   }
 
+  onAddCharacteristic(): void {
+    this.characteristicsForm.push(createCharacteristicFormGroup());
+  }
+
+  onRemoveCharacteristic(index: number): void {
+    this.characteristicsForm.removeAt(index);
+  }
+
   onSaveClick(): void {
-    this.save.emit(this.characteristics);
+    this.characteristicsForm.markAllAsTouched();
+    if (this.characteristicsForm.invalid) {
+      return;
+    }
+
+    this.save.emit(this.characteristicsForm.getRawValue());
+  }
+
+  trackByIndex(index: number): number {
+    return index;
+  }
+
+  private buildForm(): void {
+    this.characteristicsForm.clear();
+    this.characteristics.forEach((characteristic) => {
+      this.characteristicsForm.push(
+        createCharacteristicFormGroup({
+          id: characteristic.id ?? undefined,
+          name: characteristic.name ?? undefined,
+          value: characteristic.value ?? undefined,
+        })
+      );
+    });
   }
 }
